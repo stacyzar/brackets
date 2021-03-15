cddab9ce3e4f9caa05edaeb4dc1e86d6b8ff5ba3
Anastasiya Zarubina <71637520+stacyzar@users.noreply.github.com>
2021-03-15 21:38:02 +0500

    feat: make solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strBracketsConfig = bracketsConfig.map((i) => i.join(''));
+
+  for(let i = strBracketsConfig.length - 1; i >= 0; i--){
+      if(str.includes(strBracketsConfig[i])){
+          str = str.replace(strBracketsConfig[i], '');
+          i = strBracketsConfig.length;
+      }
+  }
+  if(str){
+    return false;
+  }
+  else{
+    return true;
+  }
 }
